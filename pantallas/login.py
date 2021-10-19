244caf78a2a2a1feae3bb22e66231f2f6298a297
Rocio Laptop <rocioghillino@gmail.com>
2021-10-19 16:24:04 -0300

    integracion

--- pantallas/login.py
+++ pantallas/login.py
@@ -1,6 +1,5 @@
 userbase = "s" #Usuario y contraseñas creadas solamente para verificar el correcto funcionamiento
 passbase = "s123"
-
 #------------------------------------------------------Login-----------------------------------------------------------
 def pedircuenta():
     user = input("Introducir usuario: ")
@@ -8,19 +7,24 @@ def pedircuenta():
     return user,pas
 
 def validarusuario(usuario, contraseña, usuariobase, passbase):
+    # verifica si el usuario puede ingresar al sistema; si todo bien ok va a ser True, sino false
     cont = 1
-    validación = True
-    while validación == True:
+    validacion = True
+    ok = True
+    while validacion == True:
         if usuario == usuariobase and contraseña == passbase:
-            validación = False
+            validacion = False
             print("Iniciando sesión...\n" "...\n" "Sesión Iniciada!")
         elif cont == 3:
-            validación = False
+            validacion = False
+            ok = False
             print("Se intentó el maximo numero de intentos, se ha bloqueado el inicio de sesión...")
         else:
             print("El usuario y contraseña no son validos")
             cont = cont + 1
             usuario,contraseña = pedircuenta()
+    return ok
 
-user1,pas1 = pedircuenta()
-validarusuario(user1,pas1, userbase, passbase)
+def main():
+    user1,pas1 = pedircuenta()
+    return validarusuario(user1,pas1, userbase, passbase)
--- proyecto.py
+++ proyecto.py
@@ -1,18 +1,31 @@
 # app principal
 import pantallas.reg_paciente as registro_p
 import pantallas.menu as menu
+import pantallas.login as log
+import pantallas.mod_paciente as mod_p
+from time import sleep
 import os
 clear = lambda: os.system('cls')
 
+# LOGIN
 clear()
-opcion = menu.consulta_opcion()
+ingreso = log.main()
+sleep(2)
 
-while opcion != 0:
-    clear()
-    if opcion == 4:
-        registro_p.main()
-    else:
-        pass
-        # le sigue el resto de opciones
+# MENU DE OPCIONES
+if ingreso:
     clear()
     opcion = menu.consulta_opcion()
+    while opcion != 0:
+        clear()
+        # REGISTRO DE PACIENTES
+        if opcion == 4:
+            registro_p.main()
+        # MODIFICACIÓN DE PACIENTES
+        elif opcion == 5:
+            mod_p.main()
+        else:
+            pass
+            # le sigue el resto de opciones
+        clear()
+        opcion = menu.consulta_opcion()
